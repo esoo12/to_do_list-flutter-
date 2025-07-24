c7a00076b6212593e71648bbab535ad9ef3f901b
esoo12 <esraamohsan239@gmail.com>
2025-07-24 14:45:54 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+//import 'home.dart';
+import 'welcome_page.dart'; 
 void main() {
   runApp(const MainApp());
 }
@@ -10,11 +11,10 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home:WelcomePage(),
+      
     );
   }
 }
+
